7594988e9b4d9c314ce2a6bc27c0e2e7cd6aedad
Jemel Williams <jwilliams25400@yahoo.com>
2021-08-31 20:38:18 -0400

    saving js file updates

---
+++ Employee_code/lib/Engineer.js
@@ -0,0 +1,19 @@
+// figure out what to do with this 
+ const Employee = require("./Employee");
+ 
+ class Engineer extends Employee {
+
+   constructor(name, id, email, github) {
+     super(name, id, email);
+     this.github = github;
+   }
+
+   getRole() {
+     return "Engineer";
+   }
+
+   getGithub() {
+     return this.github;
+   }
+ }
+ module.exports = Engineer;
